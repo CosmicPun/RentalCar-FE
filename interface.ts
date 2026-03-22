792c02ce9e68c2352608640c9f51c8b5cf7ec6b9
CosmicPun <41039@chs.ac.th>
2026-03-23 05:15:00 +0700

    add data model

--- interface.ts
+++ interface.ts
@@ -1,27 +1,90 @@
-interface VenueItem {
-    _id: string,
-    name: string,
-    address: string,
-    district: string,
-    province: string,
-    postalcode: string,
-    tel: string,
-    picture: string,
-    dailyrate: number,
-    __v: number,
-    id: string
-  }
-  
-  interface VenueJson {
-    success: boolean,
-    count: number,
-    pagination: Object,
-    data: VenueItem[]
-  }
+export interface UserItem {
+  _id: string;
+  name: string;
+  email: string;
+  role: 'user' | 'admin';
+  telephone: string;
+  createdAt: string;
+}
 
-  interface BookingItem {
-    nameLastname: string;
-    tel: string;
-    venue: string;
-    bookDate: string;
-  }
+export interface ProviderItem {
+  _id: string;
+  id: string;
+  name: string;
+  address: string;
+  district: string;
+  province: string;
+  postalcode: string;
+  tel: string;
+  region: string;
+  picture: string;
+  dailyrate: number;
+}
+
+export interface ProviderJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: ProviderItem[];
+}
+
+export interface SingleProviderJson {
+  success: boolean;
+  data: ProviderItem;
+}
+
+export interface CarItem {
+  _id: string;
+  id: string;
+  licensePlate: string;
+  brand: string;
+  model: string;
+  year: number;
+  color: string;
+  transmission: 'Automatic' | 'Manual';
+  fuelType: 'Gasoline' | 'Diesel' | 'Electric' | 'Hybrid';
+  available: boolean;
+  provider: string | ProviderItem;
+  rentPrice: number;
+}
+
+export interface CarJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: CarItem[];
+}
+
+export interface SingleCarJson {
+  success: boolean;
+  data: CarItem;
+}
+
+export interface BookingItem {
+  _id: string;
+  bookingDate: string;
+  returnDate: string;
+  totalCost: number;
+  user: string | UserItem;
+  car: string | CarItem;
+  provider: string | ProviderItem;
+  createdAt: string;
+}
+
+export interface BookingJson {
+  success: boolean;
+  count: number;
+  data: BookingItem[];
+}
+
+export interface SingleBookingJson {
+  success: boolean;
+  data: BookingItem;
+}
+
+export interface LocalBookingItem {
+  nameLastname: string;
+  tel: string;
+  provider: string;
+  bookDate: string;
+}
